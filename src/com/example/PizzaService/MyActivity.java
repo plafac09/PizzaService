ae2345b773b1ea9d418c8e4c3383842076b18268
Nauschi <philipp.n97@live.at>
2014-12-04 13:52:12 +0100

    added data.csv mit Pizza Testdaten Rocco ist gay

--- src/com/example/PizzaService/MyActivity.java
+++ src/com/example/PizzaService/MyActivity.java
@@ -2,14 +2,19 @@ package com.example.PizzaService;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class MyActivity extends Activity{
+
+    private Button button;
+    private ProgressBar progressBar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
     }
 }
